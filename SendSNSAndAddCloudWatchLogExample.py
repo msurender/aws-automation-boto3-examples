d0520b1a2960a39c889e22505dd3d5eac4266c1a
Surender Mallugallu <msurender@gmail.com>
2020-08-10 22:06:55 +0530

    Update and rename SNSAndCloudWatchLogExample.py to SendSNSAndAddCloudWatchLogExample.py

--- SendSNSAndAddCloudWatchLogExample.py
+++ SendSNSAndAddCloudWatchLogExample.py
@@ -6,27 +6,6 @@ from time import gmtime
 
 class AWSAutomation:
 
-	def invoke_build(self,buildProjectName):
-
-		client = boto3.client(service_name='codebuild', region_name='us-east-1')
-		new_build = client.start_build(projectName=buildProjectName)
-		buildId = new_build['build']['id']
-
-		# there is no waiter for code build, so for now we need to have our own loop
-		counter = 0
-		while counter < 12:   #capped this, so it just fails if it takes too long
-			time.sleep(180)
-			counter = counter + 1
-			theBuild = client.batch_get_builds(ids=[buildId])
-			buildStatus = theBuild['builds'][0]['buildStatus']
-			
-			if buildStatus == 'SUCCEEDED':
-				break
-			elif buildStatus == 'FAILED' or buildStatus == 'FAULT' or buildStatus == 'STOPPED' or buildStatus == 'TIMED_OUT':
-				break
-		
-		return buildStatus
-
 	def sendSNS(self,jsonResponse):
 		# NOTE - add sns topic ARN - Update below value with your sns arn
 		snsTopicArn = 'arn:aws:sns:us-east-1:123456789:codebuild'		
@@ -77,32 +56,14 @@ class AWSAutomation:
 		response = client.put_log_events(**event_log)
 		print(response)
 
-def start_time_():    
-	start_time = time.time()
-	return(start_time)
-
-def end_time_():
-	end_time = time.time()
-	return(end_time)
-    
-def execution_time(start_time,end_time):
-	return(strftime("%H:%M:%S",gmtime(int('{:.0f}'.format(float(str((end_time-start_time))))))))
-
 def main():
 
-	start_time = start_time_()
-
 	auto = AWSAutomation()
-	# pass the build name here.
-	respV = auto.invoke_build("BuildName")
-	
-	end_time = end_time_()
 	
 	# create a json message for build status and send an sns notification and also add to cloud watch logs
 	finalMessage = {
-		"Status" : "Success",
-		"Message" : "Code Build Successful",
-		"ExecutionTime": execution_time(start_time,end_time)
+		"Status" : "Status",
+		"Message" : "Message to be sent"
 	}
 			
 	print(json.dumps(finalMessage));
